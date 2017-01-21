faa5523256e17aa763aff573560c52ef6122dec8
Rohan Subramaniam <rcsubra2@illinois.edu>
2017-01-21 12:56:52 -0500

    yeet

--- discussions/urls.py
+++ discussions/urls.py
@@ -3,11 +3,16 @@ from django.contrib.auth.views import login
 
 from . import views
 
+app_name = 'discussions'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^view/(?P<user_id>\w+)/$', views.rep_view, name='rep_view'),
     url(r'^login/$', 'django.contrib.auth.views.login', {
         'template_name': 'discussions/login.html'
+    }),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {
+        'template_name': 'discussions/logged_out.html'
     }),
     url(r'^sign_up/$', views.sign_up, name = 'sign_up'),
 ]
--- discussions/views.py
+++ discussions/views.py
@@ -17,7 +17,9 @@ from .forms import RegistrationForm
 
 from django.template import RequestContext
 
-from .models import AppUser
+from .models import AppUser, Politician, Issue
+
+from django.db.models import Q
 
 def sign_up(request):
     if request.method == 'POST':
@@ -28,11 +30,35 @@ def sign_up(request):
                     last_name=form.cleaned_data['last_name'])
             app_user = AppUser(user=per, city_name=form.cleaned_data['city'], state=form.cleaned_data['state'])
             app_user.save()
-            return HttpResponse(reverse('discussions:index'))
+            return render_index(app_user, None, request)
     form = RegistrationForm()
     variables = RequestContext(request, {'form': form})
-    return render_to_response('discussions/register.html',variables)
+    return render_to_response('discussions/register.html', variables)
 
 @login_required(login_url='/discussions/login/')
 def index(request):
-    return HttpResponse("You at the index")
+    app_user = AppUser.objects.get(user=request.user)
+    return render_index(app_user, None, request)
+
+@login_required(login_url='/discussions/login/')
+def rep_view(request, user_id):
+    app_user = AppUser.objects.get(user=request.user)
+    u = User.objects.get(username=user_id)
+    au = AppUser.objects.get(user=u)
+    p = Politician.objects.get(user=au)
+    return render_index(app_user, p, request)
+
+
+@login_required(login_url='/discussions/login/')
+def render_index(app_user, rep, request):
+    template = loader.get_template('discussions/index.html')
+    polys = Politician.objects.filter(Q(rep_location=app_user.city_name) | Q(rep_location=app_user.state))
+    context = {
+        'user' : app_user,
+        'reps' : polys,
+    }
+    if(rep != None):
+        issues = Issue.objects.filter(politician=rep)
+        context['poly'] = rep
+        context['issues'] = issues
+    return HttpResponse(template.render(context, request))
