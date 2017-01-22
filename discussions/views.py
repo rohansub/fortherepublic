36789031950471d73a7d675cfbe4f04cfbe29bdf
Rohan Subramaniam <rcsubra2@illinois.edu>
2017-01-21 20:01:27 -0500

    yeerrrp

--- discussions/views.py
+++ discussions/views.py
@@ -49,14 +49,16 @@ def rep_view(request, user_id):
         au = AppUser.objects.get(user=pu)
         p = Politician.objects.get(user=au)
         now = datetime.datetime.now()
-        print text
         i = Issue(user=au, message=text, politician=p, pub_date=now)
         i.save()
-    app_user = AppUser.objects.get(user=request.user)
-    u = User.objects.get(username=user_id)
-    au = AppUser.objects.get(user=u)
-    p = Politician.objects.get(user=au)
-    return render_index(app_user, p, request)
+        issues = p.issue_set.all()
+        return render_to_response('discussions/part_index.html', { 'issues': issues })
+    else:
+        app_user = AppUser.objects.get(user=request.user)
+        u = User.objects.get(username=user_id)
+        au = AppUser.objects.get(user=u)
+        p = Politician.objects.get(user=au)
+        return render_index(app_user, p, request)
 
 
 
